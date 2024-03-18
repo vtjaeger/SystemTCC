864a476bdab5975b9277866812978b032909743a
vtjaeger <viniciustjaeger@gmail.com>
2024-03-18 10:55:05 -0300

    changes in teacher service

--- src/main/java/com/tcc/service/ProfessorService.java
+++ src/main/java/com/tcc/service/ProfessorService.java
@@ -12,21 +12,17 @@ public class ProfessorService {
     public List<LocalDateTime> encontrarHorariosComuns(List<Professor> professores) {
         List<LocalDateTime> horariosComuns = new ArrayList<>();
 
-        // Verifica se a lista de professores não está vazia antes de prosseguir
         if (!professores.isEmpty()) {
-            // Inicialmente, assumimos que os horários disponíveis do primeiro professor são comuns a todos
             horariosComuns.addAll(professores.get(0).getHorariosDisponiveis());
 
-            // Para cada professor, restringimos os horários comuns aos horários que também estão disponíveis para o professor atual
             for (int i = 1; i < professores.size(); i++) {
                 List<LocalDateTime> horariosProfessorAtual = professores.get(i).getHorariosDisponiveis();
                 horariosComuns.retainAll(horariosProfessorAtual);
             }
+            return horariosComuns;
         } else {
-            // Lidar com o caso em que a lista de professores está vazia
-            System.out.println("A lista de professores está vazia.");
+            return null;
+            //nenhum horario comum
         }
-
-        return horariosComuns;
     }
 }
